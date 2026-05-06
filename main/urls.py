d5f189718acd6d035a5225732da80717b0257347
talhakhalidawan <talhakhalidlkp@gmail.com>
2026-05-06 16:29:42 +0500

    updated custom pages path

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from .views import home, static_page_detail
 
 urlpatterns = [
     path('', home, name='home'),
-    path('pages/<slug:slug>/', static_page_detail, name='static_page_detail'),
+    path('<slug:slug>/', static_page_detail, name='static_page_detail'),
 ]
